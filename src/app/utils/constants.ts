2234eba3c2127ba32b85a9ccfbb422d48b2cfac5
Hizir Cagin Bektas <hizircaginbektas@Hizirs-MacBook-Air.local>
2020-12-01 21:18:21 +0100

    undo of dummy codes

--- src/app/utils/constants.ts
+++ src/app/utils/constants.ts
@@ -4,5 +4,11 @@ export class constants{
     public static readonly PHONE_NUMBER = 'phone-number';
     public static readonly HASH_CODE = 'hash-code';
 
+    public static readonly API_SERVER = 'http://192.168.1.14:8080/api/';
+    public static readonly LOGIN = 'mobile-users/getphonenumberbyid/';
+    public static readonly REGISTER = '/mobile-users/register';
+    public static readonly SMS_CODE = '/mobile-users/getmessagecodebycitizen';
+    public static readonly HASHCODE = '/mobile-users/gethashcodebycitizen';
+
     public static readonly UI_SMSVERIFICATION_VISIBLE_NUMBER_COUNT = 3;
 }
